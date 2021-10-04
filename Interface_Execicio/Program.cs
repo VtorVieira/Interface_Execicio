ba04eb6e86dba15ef3fbc583cb6144168393a94a
vieira.vitor42@gmail.com <vieira.vitor42@gmail.com>
2021-10-04 17:56:30 -0300

    Iniciando Execicio IEntidades

---
+++ Interface_Execicio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Execicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            Console.Write("Date (dd/MM/yyyy): ");
+            Console.Write("Enter number of installments: ");
+            Console.WriteLine("Installments: ");
+        }
+    }
+}
